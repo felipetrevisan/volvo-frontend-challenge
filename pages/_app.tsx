e64c1cf349ac36a29a1aacdf9352a282781368fa
Felipe Trevisan <felipejs@gmail.com>
2023-10-04 00:05:41 -0300

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import { HelloWorld } from "../src/components/HelloWorld";
 import "../public/css/styles.css";
 import React from "react";
+import { StyleProvider, ThemePicker } from "vcc-ui";
+import { AppProps } from "next/app";
 
-function HomePage() {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
-      <HelloWorld />
+      <StyleProvider>
+        <ThemePicker variant="light">
+          <Component {...pageProps} />
+        </ThemePicker>
+      </StyleProvider>
     </React.StrictMode>
   );
 }
-
-export default HomePage;
